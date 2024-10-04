4870dd1f58d860a88d997382e1648b0178e3ca2c
frankie-powers <franklin.power21@gmaul.com>
2024-10-04 05:54:53 +0100

    deployment

---
+++ scripts/deploySliders.ts
@@ -0,0 +1,18 @@
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const MySliders = await hre.ethers.getContractFactory("Sliders");
+  const myNFT = await MySliders.deploy(deployer.address);
+
+  console.log("MyNFT deployed to:", await myNFT.getAddress());
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
